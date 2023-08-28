3a92cf9e7f701545b1e868fbec86816c60f25cea
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-08-28 14:14:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/bite-todo-react">
+      <BrowserRouter basename="/FSOn_69_bite-todo-react">
         <Global styles={GlobalStyles} />
         <App />
       </BrowserRouter>
